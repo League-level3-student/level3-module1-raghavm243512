75fe60d2f8ac0b3e42f8e85ec9cb2458c6bd8e5e
League Student <league@iMac-91.attlocal.net>
2019-06-02 10:51:02 -0700

    finished guestbook

--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -6,18 +6,32 @@ public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-
+		ArrayList<String> a = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		a.add("five strings");
 		//3. Print all the Strings using a standard for-loop
-		
+		for (int i =0;i<a.size();i++) {
+			System.out.println(a.get(i));		
+		}
 		
 		//4. Print all the Strings using a for-each loop
-		
+		for (String i : a) {
+			System.out.println(i);	
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		for (int i =0;i<a.size();i++) {
+			if(i%2==0)
+			System.out.println(a.get(i));		
+		}
 		//6. Print all the Strings in reverse order.
-		
+		for (int i =a.size()-1;i>=0;i--) {
+			if(i%2==0)
+			System.out.println(a.get(i));		
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		for (String i : a) {
+			if(i.contains("e"))
+				System.out.println(i);
+		}
 	}
 }
--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,6 +1,56 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener{
+	JFrame window;
+	JPanel panel;
+	JButton add;
+	JButton view;
+	ArrayList<String> names;
+	public static void main(String[] args) {
+		new _02_GuestBook().start();
+	}
+	public void start() {
+		names = new ArrayList<String>();
+		window = new JFrame("");
+		panel = new JPanel();
+		add =new JButton();
+		view = new JButton();
+		add.setText("add");
+		view.setText("view");
+		panel.add(add);
+		panel.add(view);
+		add.addActionListener(this);
+		view.addActionListener(this);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.add(panel);
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		window.setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonClicked = (JButton)e.getSource();
+		if (buttonClicked==add) {
+			names.add(JOptionPane.showInputDialog("Enter a name"));
+		} else if (buttonClicked==view) {
+			String a = "";
+			for (String i : names) {
+				a+=i;
+				a+="\n";
+			}
+			JOptionPane.showMessageDialog(null, a);
+		}
+		
+	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
