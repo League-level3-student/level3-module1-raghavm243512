b55cb8811c545d44739366cf91aeb0fa2a8f3bf3
League Student <league@iMac-91.attlocal.net>
2019-06-23 10:38:57 -0700

    finishedbracket matching

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -3,6 +3,7 @@ package _03_IntroToStacks;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 import org.junit.Test;
@@ -21,7 +22,18 @@ public class _03_TestMatchingBrackets {
 	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
 		Stack<String> a = new Stack<String>();
-		
+		for (int i =0;i<b.length();i++) {
+			if (b.substring(i,i+1).equals("{")) {
+				a.push("{");
+			}
+			else if (b.substring(i,i+1).equals("}")) {
+				if (a.size()>0)
+					a.pop();
+				else return false;
+			}
+		}
+		if (a.size()==0)
+			return true;
 		return false;
 	}
 
